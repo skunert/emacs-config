d13e910706637afeefc71692eda4d72c7c483444
skunert <skunert49@gmail.com>
2021-11-16 19:05:54 +0100

    Initial commit

---
+++ config.el
@@ -0,0 +1,99 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+(after! rustic
+  (map! :map rustic-mode-map :localleader ("o" #'rustic-open-dependency-file))
+  (setq rustic-lsp-server 'rust-analyzer)
+)
+
+(after! lsp-rust
+  ;; disable the eldoc stuff
+  (setq lsp-eldoc-hook nil)
+  (setq lsp-enable-symbol-highlighting nil)
+  (setq lsp-rust-analyzer-use-client-watching nil)
+  (setq lsp-auto-guess-root t)
+
+  (setq lsp-enable-file-watchers nil)
+  ;; (setq lsp-rust-analyzer-cargo-override-command [ "rust-analyzer-cargo-check" ])
+  (setq lsp-rust-analyzer-proc-macro-enable t)
+  (setq lsp-rust-analyzer-diagnostics-disabled ["unresolved-proc-macro" "unresolved-import"])
+  (setq lsp-rust-analyzer-cargo-run-build-scripts t)
+  (setq lsp-rust-analyzer-cargo-watch-command "clippy")
+  (evil-define-key 'normal rustic-mode-map
+    "J" #'lsp-rust-analyzer-join-lines)
+
+  ; Performance
+  ; Use roughly one gigabyte
+  (setq gc-cons-threshold 1000000000)
+  ; 1MB
+  (setq read-process-output-max (* 1024 1024))
+  (setq lsp-idle-delay 0.500)
+)
+
+(use-package! inertial-scroll)
+(map! "C-j" #'inertias-up)
+(map! "C-k" #'inertias-down)
+(after! inertial-scroll
+ (setq inertias-update-time 20)
+ (setq inertias-initial-velocity 120)
+ (setq inertias-friction 300)
+ (setq inertias-brake-coef 0.4)
+)
+
+
+(use-package! treemacs
+  :config
+  (treemacs-follow-mode 1)
+)
+
+
+(defun toggle-rust-sidebar ()
+  "Toggle treemacs and symbol view"
+  (treemacs)
+  (lsp-treemacs-symbols)
+)
+
+(map! :leader :desc "Open file tree and structure" "y" (lambda () (interactive) (toggle-rust-sidebar)))
+
+(setq doom-theme 'doom-oceanic-next)
+
+(setq doom-font (font-spec :family "Fira Code" :size 14))
+
+(setq display-line-numbers-type 'relative)
+
+(setq show-trailing-whitespace t)
+
+;; Enable global whitespace mode
+(global-whitespace-mode 1)
+
+(add-hook 'diff-mode-hook 'whitespace-mode)
+
+(setq whitespace-style '(face tabs tab-mark spaces space-mark trailing lines-tail))
+
+;; Make flycheck use direnv to get the correct env for finding an executable
+;; We also need to enable `envrc-mode` manually for this buffer to make sure we set the
+;; env variables for this buffer (the mode is probably enabled later).
+(setq flycheck-executable-find
+     (lambda (cmd) (envrc-mode 1)(envrc--update-env default-directory)(executable-find cmd)))
+
+(defun save-all ()
+  (interactive)
+  (save-some-buffers t))
+
+(setq auth-sources '("~/.authinfo"))
+;; Save all buffers when emacs looses the focus
+(add-hook 'focus-out-hook 'save-all)
+(add-hook 'magit-mode-hook (lambda () (magit-delta-mode +1)))
+;; Autosave to the file directly
+(auto-save-visited-mode 1)
+
+;;(after! prog-mode
+;;  (set-company-backend! 'prog-mode 'company-abbrev-code))
+
+;; Save all buffers before searching a project
+(advice-add #'+default/search-project :before (lambda (&rest _) (evil-write-all nil)))
+
+(after! company
+  ;; Trigger completion immediately.
+  (setq company-idle-delay 0))
---
+++ init.el
@@ -0,0 +1,101 @@
+;;; init.el -*- lexical-binding: t; -*-
+
+;; Copy this file to ~/.doom.d/init.el or ~/.config/doom/init.el ('doom install'
+;; will do this for you). The `doom!' block below controls what modules are
+;; enabled and in what order they will be loaded. Remember to run 'doom refresh'
+;; after modifying it.
+;;
+;; More information about these modules (and what flags they support) can be
+;; found in modules/README.org.
+
+(doom! :input
+
+       :completion
+       (company +childframe)
+       (vertico +icons)
+
+       :ui
+       doom              ; what makes DOOM look the way it does
+       doom-dashboard    ; a nifty splash screen for Emacs
+       doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       fill-column       ; a `fill-column' indicator
+       (ligatures +fira)
+       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       modeline          ; snazzy, Atom-inspired modeline, plus API
+       nav-flash         ; blink the current line after jumping
+       minimap
+       (treemacs +lsp)
+       ophints           ; highlight the region an operation acts on
+       (popup            ; tame sudden yet inevitable temporary windows
+        +all             ; catch all popups that start with an asterix
+        +defaults)       ; default popup rules
+       vc-gutter         ; vcs diff in the fringe
+       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
+       window-select     ; visually switch windows
+       workspaces        ; tab emulation, persistence & separate workspaces
+
+       :term
+       vterm
+
+       :editor
+       (evil +everywhere); come to the dark side, we have cookies
+       file-templates    ; auto-snippets for empty files
+       fold              ; (nigh) universal code folding
+       multiple-cursors  ; editing in many places at once
+       rotate-text       ; cycle region at point between text candidates
+       snippets          ; my elves. They type so I don't have to
+
+       :emacs
+       dired             ; making dired pretty [functional]
+       electric          ; smarter, keyword-based electric-indent
+       ibuffer           ; interactive buffer management
+       vc                ; version-control and Emacs, sitting in a tree
+
+       :checkers
+       syntax              ; tasing you for every semicolon you forget
+       ;; spell             ; tasing you for misspelling mispelling
+       grammar           ; tasing grammar mistake every you make
+
+       :tools
+       direnv
+       editorconfig      ; let someone else argue about tabs vs spaces
+       (eval +overlay)     ; run code, run (also, repls)
+       gist              ; interacting with github gists
+       (lookup           ; helps you navigate your code and documentation
+        +docsets)        ; ...or in Dash docsets locally
+       lsp
+       (magit +forge)             ; a git porcelain for Emacs
+       ;; pass              ; password manager for nerds
+       pdf
+
+       :lang
+       cc                ; C/C++/Obj-C madness
+       common-lisp       ; if you've seen one lisp, you've seen them all
+       data              ; config/data formats
+       emacs-lisp        ; drown in parentheses
+       ;; kotlin            ; a better, slicker Java(Script)
+       (markdown +grip)          ; writing docs for people to ignore
+       ;; nix               ; I hereby declare "nix geht mehr!"
+       (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       sh                ; she sells {ba,z,fi}sh shells on the C xor
+       (org +dragndrop +pretty +roam2 +gnuplot +journal)
+
+       :config
+       (default +bindings +smartparens))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(counsel-find-file-ignore-regexp "\\(?:^[#.]\\)\\|\\(?:[#~]$\\)\\|\\(?:^Icon?\\)")
+ '(projectile-globally-ignored-directories
+   (quote
+    ("~/.emacs.d/.local/" ".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "target")))
+ '(projectile-globally-ignored-files (quote (".DS_Store" "Icon
+" "TAGS" "target"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
